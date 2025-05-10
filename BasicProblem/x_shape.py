6337bc5b75368374c927bb6f318743ecf3048b14
Cheese-Cheese <renji.ichirou@gmail.com>
2025-05-10 14:44:45 +0530

    X Shape problem solved

---
+++ BasicProblem/x_shape.py
@@ -0,0 +1,11 @@
+#import pdb
+#pdb.set_trace()
+
+number_of_lines = int(input("Enter no. of lines of X: "))
+for row_number in range (number_of_lines):
+    for column_number in range (number_of_lines):
+        if (row_number == column_number) or ((column_number + 1) == (number_of_lines - row_number)):
+            print('*', end='')
+        else:
+            print(' ', end='')
+    print('\n')
